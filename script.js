fc08ebb5d3251b8d867f708db5c2d7605ee09b14
Vitor Brito <vitordecamposbrito01@gmail.com>
2021-03-04 22:06:21 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Comando JS referente a SIDEBAR
+
+document.querySelector(".lista").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+);
+function lista(){
+    const temJS = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector("#layout-sim").checked
+
+}
